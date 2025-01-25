7f7678a99eed7ee648714a3c5b0a3baf12851315
mmccorm1ck <mmccorm1ck@outlook.com>
2025-01-25 18:34:07 +0000

    Testing server

--- index.ts
+++ index.ts
@@ -1,1 +1,34 @@
-console.log("Hello via Bun!");
+require('dotenv').config()
+const STORAGE_PATH = "/var/tmp/";
+
+Bun.serve({
+    port: process.env.PORT,
+    async fetch(req) {
+        const reqPath = new URL(req.url).pathname;
+        if (reqPath !== "/upload") {
+            return new Response(null, {status: 404});
+        }
+        if (req.method !== "POST") {
+            return new Response(null, {status: 405});
+        }
+        if (!req.body) {
+            return new Response(null, { status: 400 });
+        }
+        const cd = req.headers.get("content-disposition");
+        if (!cd) {
+            return new Response(null, { status: 400 });
+        }
+        const cdFileName = cd.split(";")[1];
+        if (!cdFileName) {
+            return new Response(null, { status: 400 });
+        }
+        const fileName = cdFileName.split("=")[1];
+        if (!fileName) {
+            return new Response(null, { status: 400 });
+        }
+        return new Response(null, {status: 201});
+    },
+    error() {
+        return new Response(null, {status: 500});
+    }
+});
